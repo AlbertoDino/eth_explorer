c816bde4a7e56eb48a9a92b8c427cda97e3dc467
AlbertoDino <dinoitaly@gmail.com>
2025-11-17 22:18:44 +0000

    wip

--- src/eth_trace.h
+++ src/eth_trace.h
@@ -3,7 +3,6 @@
 
 #include "eth.h"
 
-
 /* -- eth_trace -- */
 
 json_object* execute_eth_trace (struct arguments *arg);
@@ -11,35 +10,30 @@ json_object* execute_eth_trace (struct arguments *arg);
 int ex_response_eth_trace (const json_object *paylaod_request, struct arguments *arg, struct t_rpcResponse *resp);
 
 struct EthcallNode {
-    
     char * transaction_hash;
     char * address_from;
     char * address_to;
-
     long long gas_used;
-
     char * call_type;       // function called name derived from event ?
     char * topic_signature; // I don't think I need indexed paramters or data (for now)
-
     char * selector;
-
     int    is_proxy;
     char * proxy_type;
-
     char ** topics;
     size_t num_topics;
-
     struct EthcallNode ** children;
     size_t num_children;
-
     json_object* data;
 };
 
 struct EthcallNode* alloc_node();
-void init_EthcallNode(struct EthcallNode* node);
-void free_ethcallNode(struct EthcallNode* node);
-struct EthcallNode* find_address_from_node(struct EthcallNode* node,const char* address);
-void add_child(struct EthcallNode* parent,struct EthcallNode* child);
 
+void init_EthcallNode(struct EthcallNode* node);
+
+void free_ethcallNode(struct EthcallNode* node);
+
+struct EthcallNode* find_address_from_node(struct EthcallNode* node,const char* address);
+
+void add_child(struct EthcallNode* parent,struct EthcallNode* child);
 
 #endif
